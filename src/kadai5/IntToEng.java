b000c7285dcb679219570f66fbebac094bf36461
moeka0531 <g13933nm@gm.tsuda.ac.jp>
2015-05-08 15:03:55 +0900

    ドライバー　中嶋 ナビゲーター　山中
    100以上の位に挑戦中。まだエラーは直ってません。

--- src/kadai5/IntToEng.java
+++ src/kadai5/IntToEng.java
@@ -3,6 +3,10 @@ package kadai5;
 import java.util.Scanner;
 
 public class IntToEng {
+	static String[] number = {"zero","one","two","three","four","five","six","seven","eight","nine"};
+	static String[] number1 = {"ten","eleven","twelve","thirteen","fourteen","fifteen","sixteen","seventeen","eighteen","nineteen"};
+	static String[] number2 = {"null","null","twenty","thirty","forty","fifty","sixty","seventy","eighty","ninety"};
+	
     // メインメソッド
     public static void main(String[] args) {
 
@@ -18,15 +22,35 @@ public class IntToEng {
     	String s = null;
     	int units = n % 10;
     	int ten = n / 10;
-    	String[] number = {"zero","one","two","three","four","five","six","seven","eight","nine"};
-    	String[] number1 = {"ten","eleven","twelve","thirteen","fourteen","fifteen","sixteen","seventeen","eighteen","nineteen"};
-    	String[] number2 = {"null","null","twenty","thirty","forty","fifty","sixty","seventy","eighty","ninety"};
-    	if(ten==0)s=number[n];
+    	int hand = n/100;
+    	
+    	if(n>0){
+    		s=hundred(hand,ten,units);
+    	} else {
+    		if(ten==0)s=number[n];
+        	else if(ten==1) s=number1[n-10];
+        	else{
+        		if(units==0)s=number2[ten];
+        		else s=number2[ten] +" "+number[units];
+        	}
+    	}
+    	
+    	/*if(ten==0)s=number[n];
     	else if(ten==1) s=number1[n-10];
     	else{
     		if(units==0)s=number2[ten];
     		else s=number2[ten] +" "+number[units];
-    	}
+    	}*/
     	return s;
     }
+    static String hundred(int hand,int ten,int units){
+    	String s = null;
+    	if(ten==0&&units==0) s=number[hand]+" hundred";
+    	else if(ten==0) s=number[hand]+" hundred"+number[units];
+    	else if(ten==1) s=number[hand]+" hundred"+number1[units];
+    	else s=number[hand]+" hundred"+number2[ten]+number[units];
+    	return s;
+    	
+    }
+    
 }
